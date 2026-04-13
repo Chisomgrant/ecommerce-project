fc3965989b1be70845dfd134da8cfaa998d50f03
Chisomeje Grant <grantchisom4@gmail.com>
2026-04-13 11:02:09 +0100

    create integration test for product component

--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -27,7 +27,7 @@ const Products = ({ product, loadCart }) => {
     <>
       <div key={product.id} className="product-container">
         <div className="product-image-container">
-          <img className="product-image" src={product.image} />
+          <img className="product-image" data-testid='product-image' src={product.image} />
         </div>
 
         <div className="product-name limit-text-to-2-lines">
@@ -37,6 +37,7 @@ const Products = ({ product, loadCart }) => {
         <div className="product-rating-container">
           <img
             className="product-rating-stars"
+            data-testid="product-rating-stars"
             src={`images/ratings/rating-${product.rating.stars * 10}.png`}
           />
           <div className="product-rating-count link-primary">
