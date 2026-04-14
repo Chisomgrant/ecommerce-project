b7a610c30177e73fc6f88ab68436d85e75954a8c
Chisomeje Grant <grantchisom4@gmail.com>
2026-04-14 08:01:09 +0100

    create integreation test for HomePage

--- src/pages/home/Products.test.jsx
+++ src/pages/home/Products.test.jsx
@@ -54,19 +54,6 @@ describe('Products component', () => {
     })
 
     it('adds a product to the cart', async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        } 
-
-        const loadCart = vi.fn();
 
         render(<Products product={product} loadCart={loadCart} />)
 
